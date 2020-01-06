3dfc098931bcf73b6718101de8343997c239ede6
buttnomaan <buttnomaan@gmail.com>
2020-01-06 02:15:13 +0000

    First from feature/f01

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const os = require('os');
 console.log("Kubia server starting...");
-console.log("From develop");
+console.log("From feature/f01");
 var handler = function(request, response) {
 console.log("Received request from " + request.connection.remoteAddress);
 response.writeHead(200);
